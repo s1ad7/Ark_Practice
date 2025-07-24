2ab8d227205864b3317320fde1b2bec1b9b052bd
unknown <iflisaad17@gmail.com>
2025-07-24 15:37:23 +0100

    empty

--- DataType.js
+++
@@ -1,7 +0,0 @@
-let FName = "Saad";
-let LName = "Ifli";
-const FullName = FName + " " + LName;
-
-console.log(FullName);
-console.log(FullName.length)
-console.log(FullName[2])
--- var.js
+++
@@ -1,8 +0,0 @@
-let temp = "25"
-if (temp >= 25 ) {
-    console.log("its so hot today")
-
-}
-    else {
-        console.log("Dont Forget to wear a jacket")
-    }
