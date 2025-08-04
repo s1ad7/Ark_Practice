455a9c38bf7621dd109a71e62bb58ac24f2e8224
s1ad <iflisaad17@gmaiL.com>
2025-08-04 22:56:42 +0100

    starting nodejs

---
+++ Operations.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+        return a + b;
+}
+
+function sub(a, b) {
+        return a - b;
+}
+
+function mult(a, b) {
+        return a * b;
+}
+
+function divid(a, b) {
+        return a / b;
+}
+
+console.log(sum(1, 5))
+console.log(sub(1, 5))
+console.log(mult(1, 5))
+console.log(divid(1, 5))
